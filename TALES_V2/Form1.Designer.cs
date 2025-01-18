299b641b265bf30954791710ef7e053063925890
panos-stack <panaskele@gmail.com>
2025-01-18 18:25:09 +0200

    First itererration

---
+++ TALES_V2/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace TALES_V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.taleListPnl = new System.Windows.Forms.FlowLayoutPanel();
+            this.picBook = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnVolume = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.lbGR_EN = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnLanguage = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBook)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.taleListPnl, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.picBook, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 1);
+            this.tableLayoutPanel1.ForeColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(1, 2);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.09179F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.90821F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1340, 583);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // taleListPnl
+            // 
+            this.taleListPnl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.taleListPnl.AutoScroll = true;
+            this.taleListPnl.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.taleListPnl.Location = new System.Drawing.Point(3, 2);
+            this.taleListPnl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.taleListPnl.Name = "taleListPnl";
+            this.tableLayoutPanel1.SetRowSpan(this.taleListPnl, 2);
+            this.taleListPnl.Size = new System.Drawing.Size(440, 579);
+            this.taleListPnl.TabIndex = 0;
+            // 
+            // picBook
+            // 
+            this.picBook.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBook.BackgroundImage = global::TALES_V2.Properties.Resources.BookCoverIcon;
+            this.picBook.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBook.Location = new System.Drawing.Point(450, 4);
+            this.picBook.Margin = new System.Windows.Forms.Padding(4);
+            this.picBook.Name = "picBook";
+            this.picBook.Size = new System.Drawing.Size(438, 476);
+            this.picBook.TabIndex = 0;
+            this.picBook.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.btnVolume);
+            this.panel1.Controls.Add(this.btnForward);
+            this.panel1.Controls.Add(this.lbGR_EN);
+            this.panel1.Controls.Add(this.btnPlay);
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Controls.Add(this.btnLanguage);
+            this.panel1.ForeColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(449, 487);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(440, 93);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnVolume
+            // 
+            this.btnVolume.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnVolume.BackgroundImage = global::TALES_V2.Properties.Resources.volumeIcon;
+            this.btnVolume.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnVolume.Location = new System.Drawing.Point(339, 12);
+            this.btnVolume.Name = "btnVolume";
+            this.btnVolume.Size = new System.Drawing.Size(50, 50);
+            this.btnVolume.TabIndex = 4;
+            this.btnVolume.UseVisualStyleBackColor = true;
+            this.btnVolume.Click += new System.EventHandler(this.btnVolume_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnForward.BackColor = System.Drawing.Color.Transparent;
+            this.btnForward.BackgroundImage = global::TALES_V2.Properties.Resources.fwIcon;
+            this.btnForward.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnForward.Location = new System.Drawing.Point(250, 12);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(50, 50);
+            this.btnForward.TabIndex = 3;
+            this.btnForward.UseVisualStyleBackColor = false;
+            // 
+            // lbGR_EN
+            // 
+            this.lbGR_EN.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbGR_EN.AutoSize = true;
+            this.lbGR_EN.BackColor = System.Drawing.Color.Transparent;
+            this.lbGR_EN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbGR_EN.ForeColor = System.Drawing.Color.BurlyWood;
+            this.lbGR_EN.Location = new System.Drawing.Point(5, 22);
+            this.lbGR_EN.Name = "lbGR_EN";
+            this.lbGR_EN.Size = new System.Drawing.Size(39, 25);
+            this.lbGR_EN.TabIndex = 2;
+            this.lbGR_EN.Text = "EN";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnPlay.BackgroundImage = global::TALES_V2.Properties.Resources.playIcon;
+            this.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPlay.Location = new System.Drawing.Point(194, 12);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(50, 50);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnBack.BackgroundImage = global::TALES_V2.Properties.Resources.bwIcon;
+            this.btnBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBack.Location = new System.Drawing.Point(138, 12);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(50, 50);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnLanguage
+            // 
+            this.btnLanguage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnLanguage.BackgroundImage = global::TALES_V2.Properties.Resources.enIcon;
+            this.btnLanguage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLanguage.Location = new System.Drawing.Point(50, 12);
+            this.btnLanguage.Name = "btnLanguage";
+            this.btnLanguage.Size = new System.Drawing.Size(50, 50);
+            this.btnLanguage.TabIndex = 0;
+            this.btnLanguage.UseVisualStyleBackColor = true;
+            this.btnLanguage.Click += new System.EventHandler(this.btnLanguage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TALES_V2.Properties.Resources.bgIcon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1342, 586);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBook)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel taleListPnl;
+        private System.Windows.Forms.PictureBox picBook;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnVolume;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnLanguage;
+        private System.Windows.Forms.Label lbGR_EN;
+    }
+}
+
---
+++ TALES_V2/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using NAudio.Wave;
+using System.Speech.Synthesis;
+//using Windows.s
+using System.Net.Http;
+using System.Threading;
+
+namespace TALES_V2
+{
+    public partial class Form1 : Form
+    {
+        private List<Data> dataList = new List<Data>();
+        private string defSourceLang = "en";
+        bool play, mute, language;
+        int volume;
+
+        IWavePlayer waveOutDevice;
+        AudioFileReader audioFileReader;
+        AudioFileReader audioFileReader1;
+        public Form1()
+        {
+            InitializeComponent();
+            Size = new Size(1260, 600);
+            MinimumSize = new Size(800, 500);
+            SetButtonColor(this, Color.BurlyWood);
+            taleListPnl.AutoScroll = false;
+            taleListPnl.VerticalScroll.Visible = false;
+            taleListPnl.HorizontalScroll.Visible = false;
+
+            var sql = "SELECT * FROM 'Tales'";
+            try
+            {
+                using (var con = new SQLiteConnection("Data Source=DATA/DataBase.db"))
+                {
+                    con.Open();
+                    using (var com = new SQLiteCommand(sql, con))
+                    {
+                        using (var r = com.ExecuteReader())
+                            while (r.Read())
+                            {
+                                dataList.Add(new Data(r.GetInt32(0), r.GetString(1), r.GetString(2), r.GetString(3)));
+                            }
+                        com.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error " + ex.Message);
+            }
+            //string t = await getTrans(defSourceLang, "el", data.Title);
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (!play)
+            {   //pause
+                waveOutDevice.Pause();
+                btnPlay.BackgroundImage = Properties.Resources.pauseIcon;
+            }
+            else
+            {   //play
+                waveOutDevice.Play();
+                btnPlay.BackgroundImage = Properties.Resources.playIcon;
+            }
+            play = !play;
+        }
+
+        private void btnVolume_Click(object sender, EventArgs e)
+        {
+            if (!mute)
+            {   //mute
+                waveOutDevice.Volume = 0;
+                btnVolume.BackgroundImage = Properties.Resources.muteIcon;
+            }
+            else
+            {   //unmute
+                waveOutDevice.Volume = 1;
+                btnVolume.BackgroundImage = Properties.Resources.volumeIcon;
+            }
+            mute = !mute;
+        }
+
+        private void btnLanguage_Click(object sender, EventArgs e)
+        {
+            if (!language)
+            {
+                lbGR_EN.Text = "GR";
+                btnLanguage.BackgroundImage = Properties.Resources.grIcon;
+            }
+            else
+            {
+                lbGR_EN.Text = "EN";
+                btnLanguage.BackgroundImage= Properties.Resources.enIcon;
+                //translate
+            }
+            language = !language;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            play = false;
+            mute = false;
+            language = false;
+            volume = 60;
+
+            foreach (Data data in dataList)
+            {
+                Item item = new Item(data.Id, data.Title, data.Category, taleListPnl.Width);
+                item.Click += new System.EventHandler(select_Click);
+                taleListPnl.Controls.Add(item);
+            }
+        }
+
+        private async Task<string> getTrans(string sourceLang, string targetLang, string text)
+        {
+            string url = "https://api.mymemory.translated.net/get";
+            //string sourceLang = "en"; // Αγγλικά
+            //string targetLang = "el"; // gr
+
+            // Δημιουργία HTTP client
+            using (HttpClient client = new HttpClient())
+            {
+                // Δημιουργία πλήρους URL με τα query parameters
+                string requestUrl = $"{url}?q={Uri.EscapeDataString(text)}&langpair={sourceLang}|{targetLang}";
+
+                try
+                {
+                    // Κλήση της API
+                    HttpResponseMessage response = await client.GetAsync(requestUrl);
+
+                    // Βεβαιώσου ότι η κλήση πέτυχε
+                    response.EnsureSuccessStatusCode();
+
+                    // Ανάγνωση του περιεχομένου της απάντησης
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // Ανάλυση της JSON απάντησης και εξαγωγή της μετάφρασης
+                    JObject jsonResponse = JObject.Parse(responseBody);
+                    string translatedText = jsonResponse["responseData"]["translatedText"].ToString();
+
+                    // Εμφάνιση μόνο της μετάφρασης
+                    return translatedText;
+                }
+                catch (Exception e)
+                {
+                    // Διαχείριση σφαλμάτων
+                    return $"Σφάλμα: {e.Message}";
+                }
+            }
+        }
+        private void select_Click(dynamic sender, EventArgs e)
+        {
+            foreach (Item item in taleListPnl.Controls)
+                if (item != sender)
+                    if (item.s)
+                        item.unSel();
+            if (!sender.s)
+            {
+                sender.sel();
+                playStory(sender.Id);
+            }
+        }
+
+        private async void playStory(int id)
+        {
+            waveOutDevice?.Dispose();
+            audioFileReader?.Dispose();
+            using (SpeechSynthesizer synthesizer = new SpeechSynthesizer())
+            {
+                synthesizer.SetOutputToWaveFile("output.wav");
+                if (language)
+                {
+                    foreach (var voice in synthesizer.GetInstalledVoices())
+                    {
+                        //MessageBox.Show($" - {voice.VoiceInfo.Name}");
+                        var info = voice.VoiceInfo;
+                        MessageBox.Show($"Name: {info.Name}, Language: {info.Culture}, Gender: {info.Gender}");
+                    }
+                    //synthesizer.SelectVoice("Microsoft Stefanos");
+                    string text = dataList[id].History;
+                    if (text.Length > 500)
+                    {
+                        int s = text.Length / 500;
+                        int start = 0;
+                        string txt = "";
+                        for (int i = 0; i <= s; i++)
+                        {
+                            int end = start + 499;
+                            if (end > text.Length)
+                                end = text.Length;  
+                            txt += await getTrans(defSourceLang, "el", text.Substring(start, end));
+                            start = end;
+                            Thread.Sleep(2000);
+                        }
+                        synthesizer.Speak(txt);
+                    }
+                    else
+                    {
+                        string txt = await getTrans(defSourceLang, "el", dataList[id].History);
+                        Thread.Sleep(1000);
+                        synthesizer.Speak(txt);
+                    }
+
+                }
+                else
+                    synthesizer.Speak(dataList[id].History);
+            }
+
+            waveOutDevice = new WaveOut();
+            audioFileReader = new AudioFileReader("output.wav");
+            waveOutDevice.Init(audioFileReader);
+
+            waveOutDevice.Play();
+
+            btnPlay.BackgroundImage = Properties.Resources.playIcon;
+            play = true;
+        }
+
+        private void SetButtonColor(Control b, Color c)
+        {
+            foreach(Control t in b.Controls)
+            {
+                if (t is Button o)
+                    o.BackColor = Color.BurlyWood;
+                else
+                    SetButtonColor(t, c);
+            }
+        }
+    }
+
+    class Item : FlowLayoutPanel
+    {
+        public bool s = false;
+        public int Id;
+        private Label Sid;
+        private Label c;
+
+        public Item(int id, string title, string cat, int width)
+        {
+            Id = id;
+            BackColor = Color.Transparent;
+            Width = width - 19;
+            FlowDirection = FlowDirection.TopDown;
+
+            Sid = new Label()
+            {
+                AutoSize = true,
+                Text = id + 1 + ") " + title,
+                Font = new Font("French Script MT", 17),
+                ForeColor = Color.White
+            };
+
+            c = new Label()
+            {
+                Text = cat,
+                Font = new Font("French Script MT", 10),
+                ForeColor = Color.White
+            };
+            Controls.Add(Sid);
+            Controls.Add(c);
+        }
+
+        public void sel()
+        {
+            s = true;
+            BackColor = Color.FromArgb(80, 255, 255, 255);
+            Sid.BackColor = Color.Transparent;
+            c.BackColor = Color.Transparent;
+        }
+
+        public void unSel()
+        {
+            s = false;
+            c.SendToBack();
+            BackColor = Color.Transparent;
+            Sid.BackColor = Color.Transparent;
+            c.BackColor = Color.Transparent;
+        }
+    }
+
+    class Picture : PictureBox
+    {
+        public Picture(int id)
+        {
+            Width = 100;
+            Height = 100;
+        }
+    }
+
+    class Data
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string History { get; set; }
+
+        public Data(int id, string category, string title, string history)
+        {
+            Id = id;
+            Category = category;
+            Title = title;
+            History = history;
+        }
+    }
+}
